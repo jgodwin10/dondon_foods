36fd5597b98082170acef2237a934f9562c2ba21
jgodwin10 <easypeasy725@gmail.com>
2024-07-20 08:04:48 +0100

    COMMIT

--- src/Components/Payment.jsx
+++ src/Components/Payment.jsx
@@ -5,12 +5,12 @@ import Bank from "../../public/bank.png";
 
 const Payment = () => {
   return (
-    <div className="flex flex-col items-center gap-8 py-10">
+    <div className="flex flex-col items-center px-8 gap-8 py-10">
       <button className="uppercase hover:scale-[1.1] shadow-2xl duration-500 bg-black py-[12px] px-[16px] w-[205px] rounded-[99px] font-semibold text-white ">
         Make Payment
       </button>
 
-      <p className="text-[15px] md:text-[20px]">
+      <p className="text-[15px] text-center md:text-[20px]">
         We have various payment options to suit your needs
       </p>
 
